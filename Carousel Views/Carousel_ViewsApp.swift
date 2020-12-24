adb29f7972e788bfef2d9d0eb0369f127b15d590
Abdul R. Arraisi <itsarraisi@gmail.com>
2020-12-25 06:28:10 +0700

    Initial Commit

---
+++ Carousel Views/Carousel_ViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carousel_ViewsApp.swift
+//  Carousel Views
+//
+//  Created by Abdul R. Arraisi on 25/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Carousel_ViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Carousel Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Carousel Views
+//
+//  Created by Abdul R. Arraisi on 25/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
